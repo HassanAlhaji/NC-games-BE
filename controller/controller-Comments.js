1c2aa74258f12d70636787e1298df71abb3625b9
HassanAlhaji <halhaji84@gmail.com>
2023-01-13 13:57:48 +0000

    format code

--- controller/controller-Comments.js
+++ controller/controller-Comments.js
@@ -1,40 +1,39 @@
-const { fetchComments , createComment, removCOmment} = require("../model/model-Comments")
 const {
-  fetchReviewById,
-  reviewExists,
-} = require("../model/model-Reviews");
+  fetchComments,
+  createComment,
+  removCOmment,
+} = require("../model/model-Comments");
+const { fetchReviewById, reviewExists } = require("../model/model-Reviews");
 
-exports.getComments = (req, res, next)=>{
-const {review_id} = req.params
-fetchComments(review_id).then(comments=>{
-    res.status(200).send({comments})
-}).catch((err)=>{
-    next(err)
-})
-
-}
+exports.getComments = (req, res, next) => {
+  const { review_id } = req.params;
+  fetchComments(review_id)
+    .then((comments) => {
+      res.status(200).send({ comments });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
 
 exports.postComment = (req, res, next) => {
-  
   const reviewId = req.params.review_id;
   const username = req.body.username;
   const body = req.body.body;
 
-      createComment(username, reviewId, body)
-        .then((comment) => {
-          res.status(201).send({ comment });
-        })
-        .catch((error) => {
-          next(error);
-        });
+  createComment(username, reviewId, body)
+    .then((comment) => {
+      res.status(201).send({ comment });
+    })
+    .catch((error) => {
+      next(error);
+    });
 };
 
-exports.deleteComment =(req, res, next)=>{
-const {comment_id} = req.params
-removCOmment(comment_id).then(comment=>{
-  console.log(comment);
-  res.status(204).send(comment)
-
-})
-
-}
+exports.deleteComment = (req, res, next) => {
+  const { comment_id } = req.params;
+  removCOmment(comment_id).then((comment) => {
+    console.log(comment);
+    res.status(204).send(comment);
+  });
+};
--- model/model-Comments.js
+++ model/model-Comments.js
@@ -1,35 +1,42 @@
-const db = require('../db/connection')
-const comments = require('../db/data/test-data/comments')
-exports.fetchComments = (reviewId)=>{
-    if(isNaN(reviewId)){
-        return Promise.reject({
-            msg:"bad request",
-            status:400
-        })
-    }
-return db.query(`SELECT comment_id, votes, created_at, author, body, review_id FROM comments WHERE  review_id = $1 
-ORDER BY comment_id DESC;`,[reviewId]).then((result)=>{
-    if (result.rowCount ===0){
-        return []
-    }
-    return result.rows
-})
-}
+const db = require("../db/connection");
+const comments = require("../db/data/test-data/comments");
+
+exports.fetchComments = (reviewId) => {
+  if (isNaN(reviewId)) {
+    return Promise.reject({
+      msg: "bad request",
+      status: 400,
+    });
+  }
+  return db
+    .query(
+      `SELECT comment_id, votes, created_at, author, body, review_id FROM comments WHERE  review_id = $1 
+ORDER BY comment_id DESC;`,
+      [reviewId]
+    )
+    .then((result) => {
+      if (result.rowCount === 0) {
+        return [];
+      }
+      return result.rows;
+    });
+};
+
 exports.createComment = (username, reviewId, body) => {
   let query =
     "INSERT INTO comments (author, review_id, body, created_at) VALUES ($1, $2, $3, current_timestamp) returning *;";
 
+  return db.query(query, [username, reviewId, body]).then((result) => {
+    return result.rows[0];
+  });
+};
+
+exports.removCOmment = (comment_id) => {
   return db
-    .query(query, [username, reviewId, body])
+    .query(`DELETE FROM comments WHERE comment_id =$1 RETURNING *;`, [
+      comment_id,
+    ])
     .then((result) => {
       return result.rows[0];
-    })
+    });
 };
-exports.removCOmment =(comment_id)=>{
-
-  return db.query(`DELETE FROM comments WHERE comment_id =$1 RETURNING *;`,[comment_id]).then(result =>{
-    return result.rows[0]
-  })
-
-
-}
